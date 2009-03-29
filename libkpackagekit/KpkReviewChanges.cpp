961f1d5a236bd9025613605a0e6921e79c169e73
Daniel Nicoletti <dantti85-kde@yahoo.com.br>
2009-03-29 17:48:51 +0000

    Fixed license headers on files
    svn path=/trunk/playground/sysadmin/kpackagekit/; revision=946573

--- libkpackagekit/KpkReviewChanges.cpp
+++ libkpackagekit/KpkReviewChanges.cpp
@@ -13,7 +13,9 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; If not, see <http://www.gnu.org/licenses/>.  *
+ *   along with this program; see the file COPYING. If not, write to       *
+ *   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,  *
+ *   Boston, MA 02110-1301, USA.                                           *
  ***************************************************************************/
 
 #include "KpkReviewChanges.h"
