e487a22d30c335253c1d2d4f3000fad731e314cb
Daniel Nicoletti <dantti85-pk@yahoo.com.br>
2011-06-03 10:52:39 -0300

    more missing files

---
+++ Sentinel/InfoWidget.cpp
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Daniel Nicoletti                                *
+ *   dantti85-pk@yahoo.com.br                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; see the file COPYING. If not, write to       *
+ *   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,  *
+ *   Boston, MA 02110-1301, USA.                                           *
+ ***************************************************************************/
+
+#include "InfoWidget.h"
+
+#include "ui_InfoWidget.h"
+
+#include <KpkSimulateModel.h>
+#include <KpkRequirements.h>
+#include <KpkStrings.h>
+#include <KpkMacros.h>
+
+#include <KLocale>
+#include <KMessageBox>
+#include <KMimeType>
+
+#include <KDebug>
+
+#include <QWeakPointer>
+#include <QFileInfo>
+#include <QCoreApplication>
+
+#include <Daemon>
+
+#include "FilesModel.h"
+
+InfoWidget::InfoWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::InfoWidget)
+{
+    ui->setupUi(this);
+    ui->iconL->setPixmap(KIcon("dialog-warning").pixmap(128, 128));
+}
+
+InfoWidget::~InfoWidget()
+{
+    delete ui;
+}
+
+void InfoWidget::setIcon(const KIcon &icon)
+{
+    ui->iconL->setPixmap(icon.pixmap(128, 128));
+}
+
+void InfoWidget::setTitle(const QString &title)
+{
+    if (title.isNull()) {
+        ui->titleL->hide();
+    } else {
+        ui->titleL->show();
+        ui->titleL->setText(title);
+    }
+}
+
+void InfoWidget::setDescription(const QString &description)
+{
+    ui->descriptionL->setText(description);
+}
+
+#include "InfoWidget.moc"
---
+++ Sentinel/InfoWidget.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Daniel Nicoletti                                *
+ *   dantti85-pk@yahoo.com.br                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; see the file COPYING. If not, write to       *
+ *   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,  *
+ *   Boston, MA 02110-1301, USA.                                           *
+ ***************************************************************************/
+
+#ifndef INFO_WIDGET_H
+#define INFO_WIDGET_H
+
+#include <QWidget>
+#include <QAbstractItemModel>
+#include <KIcon>
+
+namespace Ui {
+    class InfoWidget;
+}
+
+class FilesModel;
+class InfoWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    InfoWidget(QWidget *parent = 0);
+    ~InfoWidget();
+
+    void setTitle(const QString &title);
+    void setDescription(const QString &description);
+    void setIcon(const KIcon &icon);
+
+private:
+    Ui::InfoWidget *ui;
+};
+
+#endif
