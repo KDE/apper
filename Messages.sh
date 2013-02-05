f1d879482b65c6dc7ea8005accde4253c0ae7b3a
Lukáš Tinkl <lukas@kde.org>
2013-02-05 14:35:09 +0100

    fix message extraction

--- Messages.sh
+++ Messages.sh
@@ -3,4 +3,4 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/apper.pot
 rm -f rc.cpp
 
-$XGETTEXT `find . -name \*.qml` -o $podir/plasma_package_updater.pot
+$XGETTEXT `find . -name \*.qml -o -name "*.cpp"` -o $podir/plasma_package_updater.pot
