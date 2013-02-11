02b73b3ac9d8bb39b4fef1ac12a67ea56135823f
Pino Toscano <pino@kde.org>
2013-02-11 17:41:46 +0100

    i18n: fix message extraction
    move the extraction of plasmoid message to an own Messages.sh,
    and filter out plasmoid sources from the general Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/apper.pot
+$XGETTEXT `find . -name \*.cpp | grep -v '/plasmoid/'` -o $podir/apper.pot
 rm -f rc.cpp
-
-$XGETTEXT `find . -name \*.qml -o -name "plasmoid/*.cpp"` -o $podir/plasma_package_updater.pot
