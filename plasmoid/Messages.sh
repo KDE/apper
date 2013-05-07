1b781c83d7c33cbcede979512375ecad092e1231
Lukáš Tinkl <lukas@kde.org>
2013-05-07 17:43:34 +0200

    don't join

--- plasmoid/Messages.sh
+++ plasmoid/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $XGETTEXT `find . -name \*.cpp` -o $podir/plasma_applet_updater.pot
-$XGETTEXT `find package -name '*.qml'` -j -L Java -o $podir/plasma_applet_org.packagekit.updater.pot
+$XGETTEXT `find package -name '*.qml'` -L Java -o $podir/plasma_applet_org.packagekit.updater.pot
