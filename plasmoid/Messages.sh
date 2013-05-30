a3240d2bfbb184067f173a140e8bbf7f456278c6
Albert Astals Cid <aacid@kde.org>
2013-05-30 22:47:14 +0200

    Fix Messages.sh
    No really, if there are no .cpp files, why is this needed?
    
    CCMAIL: lukas@kde.org

--- plasmoid/Messages.sh
+++ plasmoid/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.cpp` -o $podir/plasma_applet_updater.pot
 $XGETTEXT `find package -name '*.qml'` -L Java -o $podir/plasma_applet_org.packagekit.updater.pot
